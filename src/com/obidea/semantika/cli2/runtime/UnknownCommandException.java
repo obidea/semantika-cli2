65d27f950612e145ef1d7cd08fd9a49b73c27bdb
Josef Hardi <josef.hardi@gmail.com>
2014-07-28 18:41:24 +0700

    Message tidy.

--- src/com/obidea/semantika/cli2/runtime/UnknownCommandException.java
+++ src/com/obidea/semantika/cli2/runtime/UnknownCommandException.java
@@ -12,6 +12,6 @@ public class UnknownCommandException extends Exception
    @Override
    public String getMessage()
    {
-      return "Unknown command";
+      return "Unknown command."; //$NON-NLS-1$
    }
 }
